807bbb06637a24d51eef0d8446d06a06d6d86899
Jeremy Howard <j@fast.ai>
2022-10-05 16:58:29 +1000

    renum

---
+++ nbs/renum.py
@@ -0,0 +1,13 @@
+"Renumber files starting at 01"
+
+from fastcore.utils import *
+from shutil import move
+
+i = 1
+for p in sorted(Path().ls()):
+    pre = re.findall(r'^(\d+)[a-z]?_(.*)', str(p))
+    if not pre: continue
+    new = f'{i:02d}_{pre[0][1]}'
+    move(p, new)
+    i+=1
+
