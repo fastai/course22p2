0c752694443c7c4063a1874a683b1a6eda239f66
Jeremy Howard <j@fast.ai>
2022-12-10 09:51:28 +1000

    lsuv

--- miniai/activations.py
+++ miniai/activations.py
@@ -17,7 +17,7 @@ from .datasets import *
 from .learner import *
 
 # %% auto 0
-__all__ = ['set_seed', 'Hook', 'append_stats', 'Hooks', 'HooksCallback', 'get_hist', 'get_min', 'ActivationStats']
+__all__ = ['set_seed', 'Hook', 'Hooks', 'HooksCallback', 'append_stats', 'get_hist', 'get_min', 'ActivationStats']
 
 # %% ../nbs/10_activations.ipynb 4
 def set_seed(seed):
@@ -32,13 +32,7 @@ class Hook():
     def remove(self): self.hook.remove()
     def __del__(self): self.remove()
 
-def append_stats(hook, mod, inp, outp):
-    if not hasattr(hook,'stats'): hook.stats = ([],[])
-    acts = to_cpu(outp)
-    hook.stats[0].append(acts.mean())
-    hook.stats[1].append(acts.std())
-
-# %% ../nbs/10_activations.ipynb 41
+# %% ../nbs/10_activations.ipynb 42
 class Hooks(list):
     def __init__(self, ms, f): super().__init__([Hook(m, f) for m in ms])
     def __enter__(self, *args): return self
@@ -50,7 +44,7 @@ class Hooks(list):
     def remove(self):
         for h in self: h.remove()
 
-# %% ../nbs/10_activations.ipynb 45
+# %% ../nbs/10_activations.ipynb 46
 class HooksCallback(Callback):
     def __init__(self, hookfunc, mod_filter=fc.noop):
         fc.store_attr()
@@ -58,13 +52,16 @@ class HooksCallback(Callback):
     
     def before_fit(self):
         mods = fc.filter_ex(self.learn.model.modules(), self.mod_filter)
-        self.hooks = Hooks(mods, self.hookfunc)
+        self.hooks = Hooks(mods, self._hookfunc)
+
+    def _hookfunc(self, *args, **kwargs):
+        if self.learn.model.training: self.hookfunc(*args, **kwargs)
 
     def after_fit(self): self.hooks.remove()
     def __iter__(self): return iter(self.hooks)
     def __len__(self): return len(self.hooks)
 
-# %% ../nbs/10_activations.ipynb 50
+# %% ../nbs/10_activations.ipynb 51
 def append_stats(hook, mod, inp, outp):
     if not hasattr(hook,'stats'): hook.stats = ([],[],[])
     acts = to_cpu(outp)
@@ -72,16 +69,16 @@ def append_stats(hook, mod, inp, outp):
     hook.stats[1].append(acts.std())
     hook.stats[2].append(acts.abs().histc(40,0,10))
 
-# %% ../nbs/10_activations.ipynb 52
+# %% ../nbs/10_activations.ipynb 53
 # Thanks to @ste for initial version of histgram plotting code
 def get_hist(h): return torch.stack(h.stats[2]).t().float().log1p()
 
-# %% ../nbs/10_activations.ipynb 54
+# %% ../nbs/10_activations.ipynb 55
 def get_min(h):
     h1 = torch.stack(h.stats[2]).t().float()
-    return h1[:2].sum(0)/h1.sum(0)
+    return h1[0]/h1.sum(0)
 
-# %% ../nbs/10_activations.ipynb 57
+# %% ../nbs/10_activations.ipynb 58
 class ActivationStats(HooksCallback):
     def __init__(self, mod_filter=fc.noop): super().__init__(append_stats, mod_filter)
 
@@ -90,8 +87,16 @@ class ActivationStats(HooksCallback):
         for ax,h in zip(axes.flat, self):
             show_image(get_hist(h), ax, origin='lower')
 
-    def dead_chart(self):
-        fig,axes = get_grid(len(self), figsize=(11,5))
+    def dead_chart(self, figsize=(11,5)):
+        fig,axes = get_grid(len(self), figsize=figsize)
         for ax,h in zip(axes.flatten(), self):
             ax.plot(get_min(h))
             ax.set_ylim(0,1)
+
+    def plot_stats(self, figsize=(10,4)):
+        fig,axs = plt.subplots(1,2, figsize=figsize)
+        for h in self:
+            for i in 0,1: axs[i].plot(h.stats[i])
+        axs[0].set_title('Means')
+        axs[1].set_title('Stdevs')
+        plt.legend(fc.L.range(self))
