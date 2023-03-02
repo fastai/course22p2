3367033357ce6bb9daf9b367862606ebcac6331d
Jeremy Howard <j@fast.ai>
2023-03-02 16:27:22 +1000

    lsun

--- miniai/learner.py
+++ miniai/learner.py
@@ -47,7 +47,8 @@ def to_cpu(x):
     if isinstance(x, Mapping): return {k:to_cpu(v) for k,v in x.items()}
     if isinstance(x, list): return [to_cpu(o) for o in x]
     if isinstance(x, tuple): return tuple(to_cpu(list(x)))
-    return x.detach().cpu()
+    res = x.detach().cpu()
+    return res.float() if res.dtype==torch.float16 else res
 
 # %% ../nbs/09_learner.ipynb 35
 class MetricsCB(Callback):
